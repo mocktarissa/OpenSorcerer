59224efc84126f81acb929ba83f049773ccba4b6
mocktarissa <44744049+mocktarissa@users.noreply.github.com>
2020-08-11 04:12:52 +0300

    Working on routes --Added CRUD OPERATIONS ON ARTICLE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,24 @@ class ArticlesController < ApplicationController
         @article = Article.all
     end
     def create
-        
+        @article =Article.new(article_params)
+        @article.save
     end
     def new
-        @article= Article.new(params[:article])
+        
     end
+    def edit
+    end 
 
+    def update
+    end 
+    def destroy
+
+    end 
+    private
+    
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
     
 end
