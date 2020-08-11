c999229eb128dc863c88d82b8ebaec660b41c304
mocktarissa <44744049+mocktarissa@users.noreply.github.com>
2020-08-12 02:40:18 +0300

    Working on routes -MADE THE CODE DRY AND CLEAN

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def show
-        @article = Article.find(params[:id])
+       
     end
 
     def index
@@ -21,11 +22,11 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def edit
-        @article = Article.find(params[:id])
+       
     end 
 
     def update
-        @article =Article.find(params[:id])
+        
         
         if(@article.update(article_params))
         flash.notice="Article was updated successfuly"    
@@ -36,7 +37,7 @@ class ArticlesController < ApplicationController
         end
     end 
     def destroy
-        @article =Article.find(params[:id])
+        
         @article.destroy
         redirect_to articles_path
             
@@ -47,4 +48,7 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
     end
     
+    def set_article 
+        @article =Article.find(params[:id])
+    end
 end
